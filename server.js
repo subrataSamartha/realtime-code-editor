026d61073acda40f0a159f511372c4eb3beb62c1
Subrata Samartha <subrata.samartha2019@gift.edu.in>
2022-04-05 16:06:14 +0530

    final

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const http = require("http");
+const path = require("path");
 const { Server } = require("socket.io");
-const ACTIONS = require('./src/Actions')
+const ACTIONS = require("./src/Actions");
 
 const server = http.createServer(app);
 const io = new Server(server);
+
+app.use(express.static("build"));
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 const userSocketMap = {};
 
@@ -19,8 +25,7 @@ function getAllConnectedClients(roomId) {
       };
     }
   );
-
-} 
+}
 
 io.on("connection", (socket) => {
   console.log("socket connected", socket.id);
@@ -32,36 +37,34 @@ io.on("connection", (socket) => {
     console.log(clients);
 
     clients.forEach(({ socketId }) => {
-      io.to(socketId).emit(ACTIONS.JOINED,{
+      io.to(socketId).emit(ACTIONS.JOINED, {
         clients,
         username,
         socketId: socket.id,
-      })
-    })
+      });
+    });
   });
 
-  socket.on(ACTIONS.CODE_CHANGE, ({roomId, code}) => {
-    socket.in(roomId).emit(ACTIONS.CODE_CHANGE, {code});
-  })
-  
-  socket.on(ACTIONS.SYNC_CODE, ({socketId, code}) => {
-    io.to(socketId).emit(ACTIONS.CODE_CHANGE, {code});
-  })
+  socket.on(ACTIONS.CODE_CHANGE, ({ roomId, code }) => {
+    socket.in(roomId).emit(ACTIONS.CODE_CHANGE, { code });
+  });
 
-  socket.on('disconnecting', () => {
+  socket.on(ACTIONS.SYNC_CODE, ({ socketId, code }) => {
+    io.to(socketId).emit(ACTIONS.CODE_CHANGE, { code });
+  });
+
+  socket.on("disconnecting", () => {
     const rooms = [...socket.rooms];
     rooms.forEach((roomId) => {
       socket.in(roomId).emit(ACTIONS.DISCONNECTED, {
-        socketId : socket.id,
+        socketId: socket.id,
         username: userSocketMap[socket.id],
-      })
-    })
+      });
+    });
     delete userSocketMap[socket.id];
     socket.leave();
-
   });
 });
-
 
 const PORT = process.env.port || 5000;
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
